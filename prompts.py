a96ee5f6329679b99d8bac4cd23f34d5b35588f6
Cyrus <61042997+cyrusmcc@users.noreply.github.com>
2022-12-09 22:54:47 -0700

    Add new prompt generation system

---
+++ prompts.py
@@ -0,0 +1,33 @@
+import random  # random time interval
+
+# Add your own topics
+topics = [
+    "ChatGPT",
+]
+
+# Add your own prompt propositions
+prompt_start = [
+    "telling me about",
+]
+
+# Add your own wildcards
+wildcard = [
+    "using a mention to a popular person in the field",
+]
+
+# Add your own tweet attributes
+attributes = [
+    "using a popular relevant hashtag",
+]
+
+# Add your own constraints
+constraints = [
+    "in less than 280 characters",
+]
+
+def get_random_prompt():
+    prompt = "give me a tweet " + random.choice(prompt_start) + " " + random.choice(
+        topics) + " " + random.choice(attributes) + " " + random.choice(wildcard) + " " + constraints[0]
+    
+    prompt = prompt.replace('"', '')
+    return prompt
--- twitterscript.py
+++ twitterscript.py
@@ -1,15 +1,19 @@
 # Import the necessary libraries
-import random  # random time interval
 import os  # for accessing environment variables
 import tweepy  # for accessing the Twitter API
 from datetime import datetime  # for getting the current date and time
-from apscheduler.schedulers.blocking import BlockingScheduler # for adding delays
-import openai  # for using the OpenAI API
+from apscheduler.schedulers.blocking import BlockingScheduler  # for adding delays
 from dotenv import load_dotenv  # for loading environment variables
+from revChatGPT.revChatGPT import Chatbot  # for using the RevChatGPT API
+from prompts import get_random_prompt
 load_dotenv()  # load environment variables
 
-# Add your OpenAI API key here
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+config = {
+    "email": os.environ.get("OPENAI_EMAIL"),
+    "password": os.environ.get("OPENAI_PASSWORD"),
+}
+
+chatbot = Chatbot(config, conversation_id=None)
 
 # Create the Twitter API object
 client = tweepy.Client(
@@ -21,38 +25,25 @@ client = tweepy.Client(
 
 output_file = open('tweets.txt', 'a')
 
-def get_random_prompt():
-    with open("prompts.txt", "r") as f:
-        prompts = f.readlines()
-        prompt = random.choice(prompts)
-        return prompt
 
-def generate_text(prompt, max_tokens=70):
-    result = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        # 1 token = 4 characters, so 70 tokens = 280 characters (the max length of a tweet)
-        max_tokens=max_tokens,
-        temperature=0.4,  # temperature is a measure of randomness
-        top_p=1,  # top_p is the probability of choosing the next word
-        frequency_penalty=0,  # frequency_penalty is the probability of repeating a word
-        presence_penalty=0  # presence_penalty is the probability of repeating a word
-    )
-
-    text = result.choices[0]['text'].replace('\n', 'n').replace('n#', '')
-    return text
+def generate_text(prompt):
+    response = chatbot.get_chat_response(prompt, output="text")
+    return response['message'].replace('"', '')
 
 # Function that posts a tweet
 def post_tweet():
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    print("Posting tweet at: ", date_time)
     prompt = get_random_prompt()
     text = generate_text(prompt)
-    output_file.write('prompt: ' + prompt + ' tweet: ' + text + 'date_time: ' + date_time)
+    
     client.create_tweet(text=text, user_auth=True)
+    
+    print("Posted tweet at: ", date_time)
+    output_file.write('prompt: ' + prompt + ' date_time: ' + date_time)
 
+post_tweet()
 # Schedule the function to run every 2-6 hours
-sched = BlockingScheduler()
-sched.add_job(post_tweet, 'interval', hours=4, jitter=7200)
-sched.start()
+#sched = BlockingScheduler()
+#sched.add_job(post_tweet, 'interval', hours=4, jitter=7200)
+# sched.start()
